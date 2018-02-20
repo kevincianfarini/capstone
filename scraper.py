3a1f04fa1dc16cfc1923522b5b2d5f420d0b6ca9
Kevin Cianfarini <kevincianfarini@gmail.com>
2018-02-20 14:38:59 -0500

    Faster threaded performance, ThreadPool instead of ProcessPool

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 import requests, warc, sys
 from bs4 import BeautifulSoup
-from multiprocessing import Pool
+from multiprocessing.pool import ThreadPool
+from itertools import chain
 
 
 class BlogArchive:
@@ -50,47 +51,52 @@ class WordPressBlog:
 
 
 
-def scrape_wordpress_article(blog, **kwargs):
-    article = ArticleArchive(**kwargs)
-    print(article)
-    blog.articles.append(article)
-
-def scrape_wordpress_page(blog, page):
+def scrape_wordpress_article(link):
     def parse(text):
         parsed = {name: text.find(attrs={'class': 'entry-%s' % name}) for name in ['content', 'meta']}
         parsed['title'] = text.find_all('header', limit=2)[1].find('h1')
         return parsed
 
-    for article in page.find_all('article'):
-        link = article.find(attrs={'class': 'entry-title'}).find('a')
-        if link:
-            response = requests.get(link['href'])
-            if response.status_code != 404:
-                html = BeautifulSoup(response.content, 'html.parser')
-                scrape_wordpress_article(blog=blog, **parse(html))
-            else:
-                print('404: %s' % link['href'])
+    if link:
+        response = requests.get(link['href'])
+        if response.status_code != 404:
+            html = BeautifulSoup(response.content, 'html.parser')
+            article = ArticleArchive(**parse(html))
+            print(article)
+            return article
+        else:
+            print('404: %s' % link['href'])
 
-def scrape_wordpress_blog(url):
+def scrape_wordpress_page(page):
+    return [
+        scrape_wordpress_article(article.find(attrs={'class': 'entry-title'}).find('a')) for 
+            article in page.find_all('article')
+    ]
+
+def scrape_wordpress_blog(url, threaded=False):
     content = requests.get(url).content
     blog_archive = BlogArchive(
         BeautifulSoup(content, 'html.parser').find('h1', attrs={'class': 'site-title'}).text
     )
-    for page in WordPressBlog(url):
-        scrape_wordpress_page(blog_archive, page)
+
+    if threaded:
+        pages = list(WordPressBlog(url))
+        with ThreadPool(len(pages)) as pool:
+            articles = pool.map(scrape_wordpress_page, pages)
+            blog_archive.articles.extend(list(chain(*articles)))
+    else:
+        for page in WordPressBlog(url):
+            blog_archive.articles.extend(scrape_wordpress_page(page))
+
     return blog_archive
 
-def scrape(line):
+def scrape(line, threaded=False):
     if 'wordpress' in line.strip():
-        return scrape_wordpress_blog(line.split('->')[0].strip())
+        return scrape_wordpress_blog(line.split('->')[0].strip(), threaded)
 
 if __name__ == '__main__':
     with open(sys.argv[1]) as input:
-        if len(sys.argv) == 3 and sys.argv[2] == '--threaded':
-            lines = input.read().splitlines()
-            with Pool(len(lines)) as p:
-                print(p.map(scrape, lines))
-        else:
-            for line in input:
-                print(scrape(line))
+        for line in input:
+                print(scrape(line, len(sys.argv) == 3 and sys.argv[2] == '--threaded'))
+            
             
