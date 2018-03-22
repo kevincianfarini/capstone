4657cd0010d94f99913c247cbdc03bfca9c54731
Kevin Cianfarini <kevincianfarini@gmail.com>
2018-03-22 13:35:06 -0400

    Initial DB revision

--- files/models.py
+++ files/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    name = models.CharField(null=False, unique=True, max_length=500)
+    author = models.CharField(null=False, unique=False, max_length=100)
+
+
+class Tag(models.Model):
+    name = models.CharField(null=False, unique=True, max_length=30)
+
+
+class BlogPost(models.Model):
+    name = models.CharField(null=False, max_length=500)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    document = models.FileField(upload_to='documents')
+    tags = models.ManyToManyField(Tag)
