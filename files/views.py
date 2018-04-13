1afaa610dc490adebc8c9838b4aa44dcb63f31bd
Andrew <aeason96@vt.edu>
2018-04-13 11:12:09 -0400

    FrontEnd

--- files/views.py
+++ files/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from files.models import BlogPost
-
+from django.views import generic
 
 class HomepageView(TemplateView):
-    template_name = 'files/index.html'
+    template_name = 'frontend/index.html'
 
 
 class ContentView(TemplateView):
@@ -14,3 +14,10 @@ class ContentView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['content'] = BlogPost.objects.get(pk=kwargs['id'])
         return context
+
+class BlogTableView(generic.ListView):
+    template_name = 'frontend/index.html'
+    context_object_name = 'all_posts'
+    def get_queryset(self):
+        return BlogPost.objects.all()
+
