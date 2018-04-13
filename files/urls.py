2bc472ccdb3cb7ecddb52ad85803021dd146245b
Kevin Cianfarini <kevincianfarini@gmail.com>
2018-04-13 12:23:32 -0400

    Fix Requirements

--- files/urls.py
+++ files/urls.py
@@ -4,5 +4,6 @@ from files.views import ContentView, ListTagAPIView, ListBlogPostAPIView
 urlpatterns = [
     path('content/<int:id>/', ContentView.as_view()),
     path('tags/', ListTagAPIView.as_view()),
+    # path /files/?tags=ta1|tag2|tag3|tag4...
     path('', ListBlogPostAPIView.as_view())
 ]
