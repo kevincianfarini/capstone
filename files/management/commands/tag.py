b754202d12f0d53ad1dc756ba797b9c9df4b583f
Kevin Cianfarini <kevincianfarini@gmail.com>
2018-04-15 00:38:45 -0400

    Optimized tagging

--- files/management/commands/tag.py
+++ files/management/commands/tag.py
@@ -3,7 +3,7 @@ from files.models import BlogPost, Tag
 from ._strip import strip
 import RAKE
 from multiprocessing.pool import ThreadPool
-
+from django.db.models import Q
 
 class Command(BaseCommand):
     help = 'generate tags an existing database of blog posts'
@@ -13,13 +13,18 @@ class Command(BaseCommand):
             r = RAKE.Rake(RAKE.SmartStopList())
             return (            
                 blog,
-                filter(lambda tag: tag[1] >= 4.0, r.run(text=strip(blog), maxWords=2))
+                [tag for tag, weight in filter(lambda tag: tag[1] >= 4.0, r.run(text=strip(blog), maxWords=2))]
             )
             
         with ThreadPool(8) as p:
             blog_tags = p.map(_tag, BlogPost.objects.all())
             print('db')
-            for blog, tags in blog_tags:
-                for tag, weight in tags:
-                    t, created = Tag.objects.get_or_create(name=tag)
-                    blog.tags.add(t)
+
+            t = map(lambda tag: Tag(name=tag), set(sum([tags for blog, tags in blog_tags], [])))
+            Tag.objects.bulk_create(t)
+
+            for blog, tags in filter(lambda blog_tag: len(blog_tag[1]) > 0, blog_tags):
+                q = Q()
+                for tag in tags:
+                    q |= Q(name=tag)
+                blog.tags.add(*Tag.objects.filter(q))
