3f6b36fdaed1edec6eaaa3b89286c5f372368839
Kevin Cianfarini <kevincianfarini@gmail.com>
2018-05-01 17:43:11 -0400

    Figured out CSV error

--- files/management/commands/_strip.py
+++ files/management/commands/_strip.py
@@ -10,3 +10,13 @@ def strip(blog):
     text = re.sub(r"\s+advertisements\s+share", '', text)
     text = text.replace('\n', '')
     return text
+
+def strip_text(body):
+    soup = BeautifulSoup(body, 'html.parser')
+    text = ' '.join([hit.strip() for hit in soup.findAll(string=True) if hit.parent.name != 'script' and not isinstance(hit, Comment)])
+    for c in ['.', ',', '!', '?', ':', ';', '"', "'", '-', '~', '(', ')', '[', ']', '|', '/', '\\', '\t']:
+        text = text.replace(c, ' ')
+    text = re.sub(r"\s+advertisement\s+share", '', text)
+    text = re.sub(r"\s+advertisements\s+share", '', text)
+    text = text.replace('\n', '')
+    return text
--- files/management/commands/export.py
+++ files/management/commands/export.py
@@ -1,16 +1,17 @@
 from django.core.management.base import BaseCommand, CommandError
 from files.models import Blog, BlogPost
 from bs4 import BeautifulSoup
-from ._strip import strip
+from ._strip import strip_text
 import os
 import pandas
 import sqlite3
 
 
 class Command(BaseCommand):
-    help = 'Export blogs as text files to ./export'
+    help = 'Export blogs as a csv file'
 
     def handle(self, *args, **kwargs):
         dat = sqlite3.connect('db.sqlite3')
         d = pandas.read_sql_query('select * from files_blogpost', dat)
-        d.to_csv('export.csv')
+        d['body'] = d.body.map(strip_text)
+        d.to_csv('export.csv', sep='\t')
